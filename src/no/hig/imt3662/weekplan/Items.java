354ffb537eedd5a4a28078f777d28eced19bdbe7
Knut Remi Lovli <knutremi@myopera.com>
2013-09-25 19:59:40 +0200

    Started thinking about how to get the user's items.

---
+++ src/no/hig/imt3662/weekplan/Items.java
@@ -0,0 +1,12 @@
+package no.hig.imt3662.weekplan;
+
+import org.json.JSONObject;
+
+public class Items extends JSONObject {
+	
+	public Items() {
+		// TODO https://api.todoist.com/API/login?user=someuser&password=somepassword
+		// TODO Then get the items.
+	}
+	
+}
