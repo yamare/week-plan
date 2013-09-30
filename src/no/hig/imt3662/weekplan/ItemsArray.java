183a4255515d3a15ec7c52d6439c8dabc4367d3c
Knut Remi Lovli <knutremi@myopera.com>
2013-09-30 21:46:45 +0200

    Added JSON testing classes ItemsArray and Items for use in the UI.

--- src/no/hig/imt3662/weekplan/ItemsArray.java
+++ src/no/hig/imt3662/weekplan/ItemsArray.java
@@ -1,23 +1,21 @@
 package no.hig.imt3662.weekplan;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class Items extends JSONObject {
-	public Items() throws JSONException {
-		
+public class ItemsArray extends JSONArray {
+	public ItemsArray() throws JSONException {
 		JSONObject item1 = new JSONObject();
 		item1.put("due_date", "2013-10-03 23:00");
 		item1.put("content", "Project deadline");
-			
+		
 		JSONObject item2 = new JSONObject();
 		item2.put("due_date", "2013-10-09 09:00");
 		item2.put("content", "Finish reading for the exam");
-			
-		JSONObject items = new JSONObject();
-		items.put("item1", item1);
-		items.put("item2", item2);
-				
+		
+		JSONArray items = new JSONArray();
+		items.put(item1);
+		items.put(item2);
 	}
-	
 }
